987fb1f6bb1efadd918e81bc6e11a27fcfc7d3dd
mohamed khaled <saklymohamedkhaled@gamil.com>
2024-06-12 23:26:17 +0200

    test

--- src/app/home-page/views/documentation/documentation/documentation.component.ts
+++ src/app/home-page/views/documentation/documentation/documentation.component.ts
@@ -4,7 +4,7 @@ import { DocumentsService } from '../../../../services/documents.service';
 @Component({
   selector: 'app-documentation',
   templateUrl: './documentation.component.html',
-  styleUrls: ['./documentation.component.css'] // Correcting styleUrl to styleUrls
+  styleUrls: ['./documentation.component.css']
 })
 export class DocumentationComponent implements OnInit {
   plus: boolean = true; // Initialize plus to true
